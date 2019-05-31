1aa6b90d52c2f04122a8a2b20705510df5727e65
Austin Caron <aus10caron@icloud.com>
2019-05-30 23:35:09 -0400

    Tablet/Phone responive CSS

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -26,4 +26,16 @@ export default {
   grid-template-columns: repeat(4, 1fr);
   height: 95vh;
 }
+
+@media (max-width: 800px) {
+  .grid-template {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (max-width: 500px) {
+  .grid-template {
+    grid-template-columns: 1fr;
+  }
+}
 </style>
